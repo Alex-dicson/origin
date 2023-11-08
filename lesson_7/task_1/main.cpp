69d383a28bf8901f857690eba07cf1158ff75fa8
Alexei Kozlov <dicson-alex@yandex.ru>
2023-11-08 16:04:30 +0300

    lesson_7 complete

---
+++ lesson_7/task_1/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+#define MODE 1
+#ifndef MODE
+#error Константа MODE не определена, необходимо определить ее! 
+#endif // !MODE
+using namespace std;
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+#if MODE == 1
+	cout << "Работаю в боевом режиме" << endl;
+	cout << "Введите число 1: ";
+	cin >> a;
+	cout << "Введите число 2: ";
+	cin >> b;
+	cout << "Результат сложения: " << add(a, b) << endl;
+#elif MODE == 0
+	cout << "Работаю в режиме тренировки" << endl;
+#else
+	cout << "Неизвестный режим. Завершение работы" << endl;
+#endif
+	return 0;
+}
+
