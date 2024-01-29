f477e201949bbb3147abf496f99b092f366d37e3
Alexei Kozlov <dicson-alex@yandex.ru>
2024-01-29 15:04:06 +0300

    lesson 3 complete

---
+++ cppl/lesson_3/task_1/task_1.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+class smart_array
+{
+private:
+    int* arr_;
+    size_t size_;
+    int last_index;
+public:
+    smart_array(size_t size) : arr_(new int[size]), size_(size)
+    {
+        last_index = 0;
+    }
+
+    ~smart_array()
+    {
+        delete[] arr_;
+    }
+
+    void add_element(int val)
+    {
+        if(size_ > last_index)
+        {
+            arr_[last_index] = val;
+            last_index++;
+        }
+        else
+        {
+            throw exception("Достигнут конец массива, добавление не возможно");
+        }
+        
+    }
+    int get_element(int index)
+    {
+        if(size_ >= index)
+        {
+            return arr_[index];
+        }
+        else
+        {
+            throw exception("Запрашиваемый идндекс за пределами массива");
+        }
+    }
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    try
+    {
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+        std::cout << arr.get_element(1) << std::endl;
+    }
+    catch(const std::exception& ex)
+    {
+        std::cout << ex.what() << std::endl;
+    }
+}
+
+
---
+++ cppl/lesson_3/task_2/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+class smart_array
+{
+private:
+    int* arr_;
+    size_t size_;
+    int last_index;
+public:
+    smart_array(size_t size) : arr_(new int[size]), size_(size)
+    {
+        last_index = 0;
+    }
+
+    smart_array(const smart_array& rhs)
+    {
+        delete[] arr_;
+        last_index = rhs.last_index;
+        size_ = rhs.size_;
+        if(rhs.arr_)
+        {
+            arr_ = new int[size_];
+            for(size_t i = 0; i < size_; i++)
+            {
+                arr_[i] = rhs.arr_[i];
+            }
+        }
+        else
+        {
+            arr_ = nullptr;
+        }
+    }
+
+    ~smart_array()
+    {
+        delete[] arr_;
+        arr_ = nullptr;
+    }
+
+    void add_element(int val)
+    {
+        if(size_ > last_index)
+        {
+            arr_[last_index] = val;
+            last_index++;
+        }
+        else
+        {
+            throw exception("ƒостигнут конец массива, добавление не возможно");
+        }
+
+    }
+    int get_element(int index)
+    {
+        if(size_ >= index)
+        {
+            return arr_[index];
+        }
+        else
+        {
+            throw exception("«апрашиваемый идндекс за пределами массива");
+        }
+    }
+
+    smart_array& operator=(const smart_array& rhs)
+    {
+        if(this != &rhs)
+        {
+            delete[] arr_;
+            last_index = rhs.last_index;
+            size_ = rhs.size_;
+            arr_ = new int[size_];
+            for(size_t i = 0; i < size_; i++)
+            {
+                arr_[i] = rhs.arr_[i];
+            }
+        }
+        return *this;
+    }
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    smart_array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
+
+    smart_array new_array(2);
+    new_array.add_element(44);
+    new_array.add_element(34);
+
+    arr = new_array;
+}
+
+
