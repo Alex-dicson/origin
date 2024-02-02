9401b18b713da59ea80188be48667bb4b2992a6a
Alexei Kozlov <dicson-alex@yandex.ru>
2024-02-02 13:10:34 +0300

    lesson 4 task 2 complete

--- cppl/lesson_4/task_2/test_lists.cpp
+++ cppl/lesson_4/task_2/test_lists.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
+#include "lists.cpp"
+#include "catch2/catch_test_macros.hpp"
+#include "Catch2/catch_session.hpp"
 
+TEST_CASE("Lists", "[task_2]")
+{
+    List test;
+
+    SECTION("Test function PushBack")
+    {
+        test.PushBack(3);
+        test.PushBack(4);
+        INFO("incorrect size after being added to the end of the list");
+        CHECK(test.Size() == 2);
+        auto val = test.PopBack();
+        INFO("incorrect deletion");
+        CHECK(val == 4);
+    }
+
+    SECTION("Test function PushFront")
+    {
+        test.Clear();
+        test.PushFront(-7);
+        test.PushFront(0);
+        test.PushFront(5);
+        test.PushFront(59);
+        INFO("incorrect size after being added to the top of the list");
+        CHECK(test.Size() == 4);
+        auto val = test.PopFront();
+        INFO("incorrect deletion");
+        CHECK(val == 59);
+    }
+
+    SECTION("Test function PopBack")
+    {
+        test.Clear();
+        INFO("deleting an item in an empty list");
+        CHECK(test.PopBack());
+        test.PushBack(8);
+        test.PushBack(13);
+        test.PushBack(28);
+        auto val = test.PopBack();
+        INFO("incorrect deletion");
+        CHECK(val == 28);
+    }
+
+    SECTION("Test function PopFront")
+    {
+        test.Clear();
+        INFO("deleting an item in an empty list");
+        CHECK(test.PopBack());
+        test.PushBack(8);
+        test.PushBack(13);
+        test.PushBack(19);
+        auto val = test.PopFront();
+        INFO("incorrect deletion");
+        CHECK(val == 8);
+    }
+
+}
 
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    return Catch::Session().run();
     return 0;
 }
